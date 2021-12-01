7a85636bb12e46db356c5ba3cc7875b55a9125e9
lschuetze <lschuetze@mpi-sws.org>
2021-12-01 19:27:47 +0100

    Day 01

---
+++ 01/main.hs
@@ -0,0 +1,20 @@
+import System.IO
+import Control.Monad
+import Data.List
+
+main = do
+  input <- parse <$> readFile "input"
+  putStrLn (show $ solve1 input)
+  putStrLn (show $ solve2 input)
+
+parse :: String -> [Int]
+parse = map read . lines
+
+solve1 :: [Int] -> Int
+solve1 xs = length $ filter (< 0) $ zipWith (-) xs (tail xs)
+
+solve2 :: [Int] -> Int
+solve2 xs = length $ filter (< 0) $ zipWith (-) windows (tail windows)
+  where
+    windows :: [Int]
+    windows = map sum $ filter (\w -> length w == 3) $ transpose $ take 3 $ tails xs
